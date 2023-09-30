9bdf1ee8652b25bbfbad7453108b8066f6a90d83
GBalatti9 <gas.balatti@gmail.com>
2023-09-30 15:27:41 -0300

    Installing sequelize and setting conection with db

---
+++ database/config/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": null,
+    "database": "db_short_url",
+    "host": "127.0.0.1",
+    "port": "3307",
+    "dialect": "mysql",
+    dialectModule: require('mysql2'),
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "db_short_url",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
---
+++ database/models/Url.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, dataTypes) => {
+
+    let alias = 'Url';
+
+    let cols = {
+        id: {
+            type: dataTypes.STRING,
+            primaryKey: true,
+            autoIncrement: true,
+        }, 
+        url: {
+            type: dataTypes.STRING,
+        }, 
+        short_url: {
+            type: dataTypes.STRING,
+        }
+    }
+
+    let config = {
+        tableName: 'url',
+        tiemstamps: false,
+    }
+
+    const Url = sequelize.define(alias, cols, config);
+    return Url;
+}
