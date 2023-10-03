4c29a6ab74f5af3208f05e3c3bb16e14f3da0182
GBalatti9 <gas.balatti@gmail.com>
2023-10-02 21:14:59 -0300

    Create a new user in db

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -1,3 +1,5 @@
+const { User } = require('../database/models');
+
 module.exports = {
     getAdmin: (req, res) => {
         res.render('loginAdmin')
@@ -8,7 +10,35 @@ module.exports = {
         res.redirect('/');
     },
 
-    getDashboard: (req, res) => {
-        res.render('adminView');
+    getDashboard: async (req, res) => {
+        try {
+            const allUsers = await User.findAll({
+                attributes: ['id', 'email', 'category']
+            })
+            res.render('adminView', {users: allUsers});
+        } catch (error) {
+            console.log(error);
+        }
+    },
+
+    postUser: async (req, res) => {
+        const { registerEmail, registerPassword, registerPasswordCheck, registerCategory } = req.body;
+        console.log({registerEmail, registerPassword, registerPasswordCheck, registerCategory});
+        try {
+            const [user, created] = await User.findOrCreate({
+                where: {
+                    email: registerEmail
+                },
+                defaults: {
+                    email: registerEmail,
+                    password: registerPassword,
+                    check_password: registerPasswordCheck,
+                    category: registerCategory,
+                }
+            })
+            res.redirect('/admin');
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,13 +1,17 @@
 const express = require('express');
-const { getAdmin, postAdmin, getDashboard } = require('../controllers/adminControllers');
+const { getAdmin, postAdmin, getDashboard, postUser } = require('../controllers/adminControllers');
 
 const router = express.Router();
+
 
 // @GET --> /admin
 router.get('/adminLogin', getAdmin);
 
 // @POST --> /admin
 router.post('/adminLogin', postAdmin);
+
+// @POST --> /createUser
+router.post('/createUser', postUser);
 
 // @GET --> /admin
 router.get('/admin', getDashboard);
