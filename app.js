be19f57ea7c5fc092ad78af1f428d17797092683
GBalatti9 <gas.balatti@gmail.com>
2023-10-03 13:33:50 -0300

    Ajax form with javascript

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express()
 
 app.use(methodOverride('_method'));
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.set('view engine', 'ejs');
--- middlewares/validations/validateCreateUser.js
+++ middlewares/validations/validateCreateUser.js
@@ -6,6 +6,13 @@ const validateCreateUser = [
     check('registerPassword')
         .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{8,}$/)
         .withMessage('La contraseña debe tener al menos 1 mayúscula, 1 minúscula, 1 carácter especial y 1 número, y ser de al menos 8 caracteres de longitud'),
+    check('registerPasswordCheck')
+        .notEmpty()
+        .withMessage('No puede estar vacía la verificación de contraseña'),
+    check('registerCategory')
+        .notEmpty()
+        .withMessage('Elija una categoría'),
+    
 ]
 
 module.exports = {
