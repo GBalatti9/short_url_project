5dcf4ac6b9ffc517266aec13e6307baec956a04b
GBalatti9 <gas.balatti@gmail.com>
2023-12-23 17:27:19 -0300

    Updating port configuration in sequelize production to deploy

--- database/config/config.js
+++ database/config/config.js
@@ -13,13 +13,17 @@ module.exports = {
     "password": null,
     "database": "database_test",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "port": "3307",
+    "dialect": "mysql",
+    dialectModule: require('mysql2'),
   },
   "production": {
     "username": "root",
     "password": null,
     "database": "db_short_url",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "port": "3307",
+    "dialect": "mysql",
+    dialectModule: require('mysql2'),
   }
 }
