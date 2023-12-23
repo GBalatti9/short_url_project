aa6dbf4a84ccf3a1f00e8a77a2566a903eec2230
GBalatti9 <gas.balatti@gmail.com>
2023-12-23 18:01:48 -0300

    Ip direction onrender

--- database/config/config.js
+++ database/config/config.js
@@ -21,7 +21,7 @@ module.exports = {
     "username": "root",
     "password": null,
     "database": "db_short_url",
-    "host": "127.0.0.1",
+    "host": "44.226.145.213",
     "port": "3307",
     "dialect": "mysql",
     dialectModule: require('mysql2'),
